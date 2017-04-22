5705a2f619951c2a30988e94f4c5ab41ac0195be
leblanc119 <leblanc119@gmail.com>
2017-04-22 12:32:59 -0500

    Flex Payment Complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -14,12 +14,26 @@ def flex_square_root
   render("calculations/flex_square_root.html.erb")
 end
 
+def flex_payment
+  @apr = params["the_number1"].to_f/100.0
+  @years = params["the_number2"].to_i
+  @principal = params["the_number3"].to_f
+
+  monthly_rate=(@apr/100)/12
+  num_payments=@years*12
+  @monthly_payment = ((monthly_rate)/(1-(1+monthly_rate)**(num_payments*-1)))*@principal
+
+  render("calculations/flex_payment.html.erb")
+end
+
 def square_form
   render("calculations/square_form.html.erb")
 end
+
 def square
   @user_number = params["number_square"].to_f
   @squared_number = @user_number**2
   render("calculations/square.html.erb")
 end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get("/flexible/square/:the_number",{:controller => "calculations", :action => "flex_square"})
   get("/flexible/square_root/:the_number",{:controller => "calculations", :action => "flex_square_root"})
+  get("/flexible/payment/:the_number1/:the_number2/:the_number3",{:controller => "calculations", :action => "flex_payment"})
 
 #Two below are for the form page
   get("/square/new",{:controller => "calculations", :action => "square_form"})
